33a9ca81b67366286e5b88311bd7a2fd98b8fc27
HUNGDT <hk2t.thaihung@gmail.com>
2019-02-14 11:25:00 +0700

    dictionary

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {TimelinesComponent} from './timelines/timelines.component';
 import {YoutubePlaylistComponent} from './youtube-playlist/youtube-playlist.component';
 import {YoutubePlayerComponent} from './youtube-player/youtube-player.component';
+import {DictionaryComponent} from './dictionary/dictionary.component';
+import {DictionarypageComponent} from './dictionarypage/dictionarypage.component';
+import {DictionarydetailComponent} from './dictionarydetail/dictionarydetail.component';
 
 const routes: Routes = [{
   path: 'timelines',
@@ -15,6 +18,16 @@ const routes: Routes = [{
       path: ':id',
       component: YoutubePlayerComponent
     }]
+  }, {
+    path: 'dictionaries',
+    component: DictionarypageComponent,
+    children: [{
+      path: ':key',
+      component: DictionarydetailComponent
+    }]
+  }, {
+    path: 'dictionary',
+    component: DictionaryComponent
   }];
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { TimelinesComponent } from './timelines/timelines.component';
 import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
 import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+import { DictionarypageComponent } from './dictionarypage/dictionarypage.component';
+import { DictionarydetailComponent } from './dictionarydetail/dictionarydetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimelinesComponent,
     YoutubePlaylistComponent,
-    YoutubePlayerComponent
+    YoutubePlayerComponent,
+    DictionaryComponent,
+    DictionarypageComponent,
+    DictionarydetailComponent
   ],
   imports: [
     BrowserModule,
